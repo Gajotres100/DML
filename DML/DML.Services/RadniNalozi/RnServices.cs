53002b031007eeab97108f75644e0d8845bf5ff8
Nikola Gaić <ngaic@combis.hr>
2019-04-12 14:14:01 +0200

    -

---
+++ DML/DML.Services/RadniNalozi/RnServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DML.Services.RadniNalozi
+{
+    public class RnServices
+    {
+    }
+}
