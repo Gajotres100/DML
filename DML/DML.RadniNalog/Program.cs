359e81665239331d02c9a0ab60155d94eb216d5b
Nikola Gaic <nikola.gaic@gmail.com>
2019-04-12 13:56:37 +0200

    Add files via upload

--- DML/DML.RadniNalog/Program.cs
+++ DML/DML.RadniNalog/Program.cs
@@ -16,7 +16,7 @@ namespace DML.RadniNalog
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RadniNalozi());
         }
     }
 }
