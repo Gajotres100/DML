866f8c0e16cafb81b9b70d6c20b34f6ea05f036d
Nikola Gaić <ngaic@combis.hr>
2019-04-12 14:08:18 +0200

    -

---
+++ DML/DML.Repository/RadniNalozi/RnRepository.cs
@@ -0,0 +1,20 @@
+﻿using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DML.Repository.RadniNalozi
+{
+    public class RnRepository
+    {
+        public void Save()
+        {
+            using (var context = new DMLEntities())
+            {
+                
+            }
+        }
+    }
+}
---
+++ DML/DML.VM/SaveRnDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DML.VM
+{
+    public class SaveRnDto
+    {
+        public int Id { get; set; }
+        public DateTime? Datum { get; set; }
+        public string VrstaRobe { get; set; }
+        public int? Kolicina { get; set; }
+        public string Izvrstitelj { get; set; }
+        public string RegOznaka { get; set; }
+        public string VrstaUsluge { get; set; }
+        public string Radiliste { get; set; }
+        public string Vozac { get; set; }
+        public string VrstaStroja { get; set; }
+        public string Narucitelj { get; set; }
+        public string Primatelj { get; set; }
+    }
+}
